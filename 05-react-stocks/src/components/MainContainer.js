0a5ee48cecd75483f3e86ae03c03dfca18d82053
betalantz <betalantz@gmail.com>
2022-05-24 10:23:20 -0700

    completes first deliv 05

--- 05-react-stocks/src/components/MainContainer.js
+++ 05-react-stocks/src/components/MainContainer.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+
+  const [stocksArr, setStocksArr] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then(res => res.json())
+      // .then(dataArr => setStocksArr(dataArr))
+      .then(setStocksArr)
+  }, [])
+  
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocksArr}/>
         </div>
         <div className="col-4">
           <PortfolioContainer />
--- 05-react-stocks/src/components/StockContainer.js
+++ 05-react-stocks/src/components/StockContainer.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({stocks}) {
+
+  const stockCards = stocks.map(stock => <Stock {...stock}/>)
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stockCards}
     </div>
   );
 }
