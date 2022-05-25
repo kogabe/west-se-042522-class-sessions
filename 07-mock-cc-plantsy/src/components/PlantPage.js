518dc31ca08fddfc32f7f24ad8fb2fac284c0ed9
betalantz <betalantz@gmail.com>
2022-05-25 14:26:27 -0700

    complete 1st deliv 07

--- 07-mock-cc-plantsy/src/components/PlantPage.js
+++ 07-mock-cc-plantsy/src/components/PlantPage.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+
+  const baseURL = "http://localhost:6001"
+
+  useEffect(() => {
+    fetch(baseURL + "/plants")
+      .then(res => res.json())
+      .then(setPlants)
+  
+
+  }, [])
+  
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants}/>
     </main>
   );
 }
