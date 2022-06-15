53aa31f1d8c9f6f3be9993906c73556b95f8c7c4
betalantz <betalantz@gmail.com>
2022-06-15 10:29:20 -0700

    complete 11

--- 11-magazines-cc-practice/app/models/magazine.rb
+++ 11-magazines-cc-practice/app/models/magazine.rb
@@ -1,3 +1,20 @@
 class Magazine < ActiveRecord::Base
-  
+
+    def self.most_popular
+        # arr = Subscription.pluck(:magazine_id)
+        # max_id = arr.max_by { |id| arr.count(id) }
+        # self.find(max_id)
+        # =======
+        # self.all.max{|a, b| a.subscriptions.length <=> b.subscriptions.length}
+        self.all.max_by{|mag| mag.subscriptions.length}
+    end
+
+    has_many :subscriptions
+    has_many :readers, through: :subscriptions
+
+    def email_list
+        # self.readers.map(&:email).join(';')
+        self.readers.pluck(:email).join(';')
+    end
+
 end
--- 11-magazines-cc-practice/app/models/reader.rb
+++ 11-magazines-cc-practice/app/models/reader.rb
@@ -1,3 +1,22 @@
 class Reader < ActiveRecord::Base
+
+    has_many :subscriptions
+    has_many :magazines, through: :subscriptions
+
+    def subscribe(magazine, price)
+        # Subscription.create(reader: self, magazine: magazine, price: price)
+        self.subscriptions.create(magazine: magazine, price: price)
+    end
+
+    def total_subscription_price
+        self.subscriptions.sum(:price)
+    end
+
+    def cancel_subscription(magazine)
+        self.subscriptions.where(magazine: magazine).destroy_all
+        # self.subscriptions.find_by(magazine: magazine).destroy
+    end
+
+    
   
 end
