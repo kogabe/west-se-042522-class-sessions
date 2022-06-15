fc9b43c6cc2607e57455d0fb9b29aa129baa21b9
betalantz <betalantz@gmail.com>
2022-06-15 16:27:03 -0700

    add comments to 12

--- 12-freebies-cc-practice/app/models/dev.rb
+++ 12-freebies-cc-practice/app/models/dev.rb
@@ -1,10 +1,10 @@
 class Dev < ActiveRecord::Base
 
-    def self.dev_with_highest_value_freebies
+    def self.dev_with_highest_value_freebies # not in deliverables
         self.all.max_by {|dev| dev.freebies.sum(:value)}
     end
 
-    def self.total_freebie_value_by_dev
+    def self.total_freebie_value_by_dev # not in deliverables
         self.all.map{|dev| "#{dev.name} has a total value of #{dev.total_freebie_value}"}
     end
 
@@ -20,7 +20,7 @@ class Dev < ActiveRecord::Base
     end
 
     def give_away(dev, freebie)
-        # if received_one?(freebie.item_name)
+        # if received_one?(freebie.item_name) # this one is not totally secure
         #     freebie.update(dev: dev)
         # else
         #     "That's stealing!!!"
@@ -35,7 +35,7 @@ class Dev < ActiveRecord::Base
         # freebie.update(dev: dev) unless freebie.dev != self
     end
 
-    def total_freebie_value
+    def total_freebie_value # not in deliverables
         self.freebies.sum(:value)
     end
 
