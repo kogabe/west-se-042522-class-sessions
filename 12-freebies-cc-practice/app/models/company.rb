91d6736fe0c247ef0edb9c2e9c989bd2ae851acd
betalantz <betalantz@gmail.com>
2022-06-15 16:26:07 -0700

    complete 12

--- 12-freebies-cc-practice/app/models/company.rb
+++ 12-freebies-cc-practice/app/models/company.rb
@@ -1,3 +1,26 @@
 class Company < ActiveRecord::Base
 
+    def self.oldest_company
+        # pluck, sort, return first one
+        # earliest = Company.pluck(:founding_year).sort.first
+        # Company.find_by(founding_year: earliest)
+        # ======
+        # self.all.min_by{|company| company.founding_year}
+        # self.all.min_by(&:founding_year) # use Ruby method min_by
+        # =======
+        # old = Company.minimum(:founding_year)
+        # Company.find_by(founding_year: old)
+        # ==========
+        self.order(:founding_year).first # using AR methods
+    end
+
+    has_many :freebies
+    has_many :devs, through: :freebies
+
+    def give_freebie(dev, item_name, value)
+        # Freebie.create(dev: dev, company: self, item_name: item_name, value: value)
+        self.freebies.create(dev: dev, item_name: item_name, value: value)
+
+    end
+
 end
--- 12-freebies-cc-practice/app/models/dev.rb
+++ 12-freebies-cc-practice/app/models/dev.rb
@@ -1,3 +1,42 @@
 class Dev < ActiveRecord::Base
 
+    def self.dev_with_highest_value_freebies
+        self.all.max_by {|dev| dev.freebies.sum(:value)}
+    end
+
+    def self.total_freebie_value_by_dev
+        self.all.map{|dev| "#{dev.name} has a total value of #{dev.total_freebie_value}"}
+    end
+
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def received_one?(item_name)
+        # self.freebies.pluck(:item_name).include?(item_name)
+        # self.freebies.any? {|f| f.item_name == item_name}
+        # item = self.freebies.find_by(item_name: item_name)
+        # item ? true : false
+        !!self.freebies.find_by(item_name: item_name)
+    end
+
+    def give_away(dev, freebie)
+        # if received_one?(freebie.item_name)
+        #     freebie.update(dev: dev)
+        # else
+        #     "That's stealing!!!"
+        # end
+        if freebie.dev == self
+            freebie.update(dev: dev)
+            self.reload
+            dev.reload
+        else
+            "That's stealing!!!"
+        end
+        # freebie.update(dev: dev) unless freebie.dev != self
+    end
+
+    def total_freebie_value
+        self.freebies.sum(:value)
+    end
+
 end
