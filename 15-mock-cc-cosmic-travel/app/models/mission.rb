3da213ec1e2e8df12a93f7fcb339a237ea96284d
betalantz <betalantz@gmail.com>
2022-07-05 11:51:59 -0700

    fix validation on Mission

--- 15-mock-cc-cosmic-travel/app/models/mission.rb
+++ 15-mock-cc-cosmic-travel/app/models/mission.rb
@@ -3,5 +3,5 @@ class Mission < ApplicationRecord
   belongs_to :planet
 
   validates :name, presence: true
-  validates :scientist, uniqueness: {scope: :planet}
+  validates :scientist, uniqueness: {scope: :name}
 end
