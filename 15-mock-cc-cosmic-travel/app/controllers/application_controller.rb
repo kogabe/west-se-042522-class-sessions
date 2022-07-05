7ed5a61594c378403ab16a25b86bc4c863f75ea9
betalantz <betalantz@gmail.com>
2022-07-05 11:27:35 -0700

    adds solution 15

--- 15-mock-cc-cosmic-travel/app/controllers/application_controller.rb
+++ 15-mock-cc-cosmic-travel/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_reponse
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def render_unprocessable_entity_reponse(exception)
+        render json: { errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+    def render_not_found_response(exception)
+        render json: {error: "#{exception.model} not found"}, status: :not_found
+    end
 end
