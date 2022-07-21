d793809dc542537f38f60bbeb164998d4cb71a25
betalantz <betalantz@gmail.com>
2022-07-20 17:16:21 -0700

    add petsSlice to 18

--- 18-react-redux-shelter/src/components/PetBrowser.jsx
+++ 18-react-redux-shelter/src/components/PetBrowser.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Pet from './Pet'
+import { useSelector } from 'react-redux'
 
 export default function PetBrowser() {
+
+    const pets = useSelector(state => state.pets)
+
+  const petCards = pets.map((pet) => (
+    <Pet key={pet.id} pet={pet} />
+  ));
+
   return (
-    <div>PetBrowser</div>
+    <>
+     
+        <header as="h3">PetBrowser</header> 
+        <div className="ui cards">{petCards}</div>
+    </>
   )
 }
